0736ecae608e098d0a15927be6d44356c8ae01c5
wangjing06 <wangjing06@kuaishou.com>
2018-11-01 17:55:54 +0800

    commit message

---
+++ src/main/java/com/example/learningspringboot/LearningSpringbootApplication.java
@@ -0,0 +1,12 @@
+package com.example.learningspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearningSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearningSpringbootApplication.class, args);
+	}
+}
